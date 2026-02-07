9c3b93f21a9e98ca17046f4783844b5efbe8f9a3
ashoksrmcem <ashoksrmcem@gmail.com>
2026-02-07 09:44:17 +0530

    push

---
+++ Models/Dto/OrderHeaderUpdateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MangoFusion_API.Models.Dto
+{
+    public class OrderHeaderUpdateDTO
+    {
+        [Required]
+        public int OrderHeaderId { get; set; }
+        public string PickUpName { get; set; } = string.Empty;
+        public string PickUpPhoneNumber { get; set; } = string.Empty;
+        public string PickUpEmail { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
